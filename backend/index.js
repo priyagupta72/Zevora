52c620a4dfd747e1cb760bd5ea8af3a3c7ebcbda
Priya Gupta <priyagupta4245@gmail.com>
2025-09-05 22:22:49 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://zevoraa.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
